2a6dfd7e387f9ad9b5f9c95afc96561ba89bf756
나병현 <nbhyun0329@gmail.com>
2024-09-13 19:07:09 +0900

    hello :: 프로젝트 생성

---
+++ src/main/kotlin/by/dairy/DaIryApplication.kt
@@ -0,0 +1,11 @@
+package by.dairy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DaIryApplication
+
+fun main(args: Array<String>) {
+    runApplication<DaIryApplication>(*args)
+}
